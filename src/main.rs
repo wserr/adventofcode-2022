b70464cb89b6b6350e73eafd22e4d71a385a3534
wserr <serruys.willem@hotmail.com>
2023-03-04 23:02:14 +0100

    play with bin folder

--- src/main.rs
+++ src/main.rs
@@ -1,42 +1,6 @@
-mod day_01;
-mod day_02;
-mod day_03;
-mod day_04;
-mod day_05;
-mod day_06;
-mod day_07;
-mod day_08;
-mod day_09;
-mod day_10;
 mod day_11;
 
 fn main() {
-    //    println!("{}", day_01::solution_1());
-    //    println!("{}", day_01::solution_2());
-    //
-    //    println!("{}", day_02::solution_1());
-    //    println!("{}", day_02::solution_2());
-    //
-    //    println!("{}", day_03::solution_1());
-    //    println!("{}", day_03::solution_2());
-    //
-    //    println!("{}", day_04::solution_1());
-    //    println!("{}", day_04::solution_2());
-
-    //    println!("{}", day_05::solution_1());
-    //    println!("{}", day_05::solution_2());
-    //
-    //println!("{}", day_06::solution_1());
-    //println!("{}", day_06::solution_2());
-    //
-    //println!("{}", day_07::solution_1());
-    //println!("{}", day_07::solution_2());
-    //
-    //println!("{}", day_08::solution_1());
-    //println!("{}", day_08::solution_2());
-    //println!("{}", day_09::solution_1());
-    //println!("{}", day_09::solution_2());
-    //println!("{}", day_10::solution_1());
-    //println!("{}", day_10::solution_2());
+    // Prints the latest solution
     println!("{}", day_11::solution_1());
 }
